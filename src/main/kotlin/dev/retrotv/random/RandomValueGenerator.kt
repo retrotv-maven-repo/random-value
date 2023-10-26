659f1bb64f72acf6806a852c788bc0c512b5ceb8
yjj8353 <yjj8353@gmail.com>
2023-10-27 00:45:41 +0900

    주석 추가

--- src/main/kotlin/dev/retrotv/random/RandomValueGenerator.kt
+++ src/main/kotlin/dev/retrotv/random/RandomValueGenerator.kt
@@ -37,25 +37,40 @@ class RandomValueGenerator(vararg charGroup: CharArray) : Generator {
         return generatedValue?.toByteArray()
     }
 
+    /**
+     * 모든 CharGroup에서 각각, 최소 하나의 값을 보장하도록 설정합니다.
+     */
     fun enableAllCharGroupLeastOne() {
         isAllCharGroupLeastOne = true
     }
 
+    /**
+     * 모든 CharGroup에서 각각, 최소 하나의 값을 보장하지 않도록 설정합니다.
+     */
     fun disableAllCharGroupLeastOne() {
         isAllCharGroupLeastOne = false
     }
 
+    /**
+     * 모든 CharGroup에서 동일한 확률로 값이 선택되도록 설정합니다.
+     */
     fun enableEqualDistribution() {
         isEqualDistribution = true
     }
 
+    /**
+     * 모든 CharGroup에서 동일하지 않은 확률로 값이 선택되도록 설정합니다.
+     */
     fun disableEqualDistribution() {
         isEqualDistribution = false
     }
 
+    /*
+     * 지정된 길이(len)과 각종 옵션 값에 근거하여 무작위 문자열을 생성하고 반환합니다.
+     */
     private fun generateValue(len: Int): String {
         val fullChars = getFullChars()
-        val allLeastOneChars = if (isAllCharGroupLeastOne) { getAllCharsGroupLeastOne() } else { null }
+        val allLeastOneChars = if (isAllCharGroupLeastOne) { getAllCharGroupLeastOne() } else { null }
 
         val sr = SecureRandom()
         var ca = CharArray(len)
@@ -83,6 +98,9 @@ class RandomValueGenerator(vararg charGroup: CharArray) : Generator {
         return String(ca)
     }
 
+    /*
+     * 모든 CharGroup을 묶어, 하나의 CharArray(char[])로 변환해 반환합니다.
+     */
     private fun getFullChars(): CharArray {
         val fullChars = CharArray(allCharGroupLeastCommonMultiple * allCharGroup.size)
         var destPos = 0
@@ -103,7 +121,11 @@ class RandomValueGenerator(vararg charGroup: CharArray) : Generator {
         return fullChars
     }
 
-    private fun getAllCharsGroupLeastOne(): CharArray {
+    /*
+     * 모든 CharsGroup에서, 각각 하나의 문자를 무작위로 뽑아 반환합니다.
+     * 예를 들어 소문자/숫자 CharGroup이 존재하는 경우, 소문자 한 글자와 숫자 한 글자를 반환합니다.
+     */
+    private fun getAllCharGroupLeastOne(): CharArray {
         val acglo = CharArray(allCharGroup.size)
         allCharGroup.forEachIndexed { i, it ->
             val sr = SecureRandom()
