775291468050d7fde924dbb8e4df2c1418762686
yjj8353 <yjj8353@gmail.com>
2024-08-15 20:00:29 +0900

    테스트 커버리지 추가

--- src/test/kotlin/dev/retrotv/random/StringGeneratorTest.kt
+++ src/test/kotlin/dev/retrotv/random/StringGeneratorTest.kt
@@ -2,8 +2,13 @@ package dev.retrotv.random
 
 import dev.retrotv.data.utils.ByteUtils.toHexString
 import dev.retrotv.random.enums.SecurityStrength
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.RepeatedTest
 import java.security.SecureRandom
 import kotlin.test.Test
+import kotlin.test.*
+
 
 class StringGeneratorTest {
 
@@ -31,5 +36,65 @@ class StringGeneratorTest {
 
         println(toHexString(randomValue1))
         println(toHexString(randomValue2))
+    }
+
+    @Nested
+    @DisplayName("isAllCharGroupLeastOne 분기 테스트")
+    inner class TestIsAllCharGroupLeastOne {
+
+        @RepeatedTest(value = 100, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+        @DisplayName("모든 CharGroup에서 각각, 최소 하나의 값을 보장하도록 설정")
+        fun test_enableAllCharGroupLeastOne() {
+            val passwordGenerator = PasswordGenerator(SecurityStrength.HIGH, SecureRandom())
+            passwordGenerator.enableAllCharGroupLeastOne()
+            passwordGenerator.generate(8)
+            val generatedValue = passwordGenerator.getValue()
+
+            assertTrue(generatedValue.contains(Regex(".*[A-Z]+")))
+            assertTrue(generatedValue.contains(Regex(".*[a-z]+")))
+            assertTrue(generatedValue.contains(Regex(".*[0-9]+")))
+            assertTrue(generatedValue.contains(Regex(".*[\\\\!@#%^&*()_+-=\\[\\]{}|;':\",./<>?~`]+")))
+        }
+
+        @Test
+        @DisplayName("모든 CharGroup에서 각각, 최소 하나의 값을 보장하지 않도록 설정")
+        fun test_disableAllCharGroupLeastOne() {
+            val passwordGenerator = PasswordGenerator(SecurityStrength.HIGH, SecureRandom())
+            passwordGenerator.disableAllCharGroupLeastOne()
+            passwordGenerator.generate(8)
+            val generatedValue = passwordGenerator.getValue()
+
+            assertNotNull(generatedValue)
+            assertEquals(8, generatedValue.length)
+        }
+    }
+
+    @Nested
+    @DisplayName("isEqualDistribution 분기 테스트")
+    inner class TestIsEqualDistribution {
+
+        @Test
+        @DisplayName("모든 CharGroup에서 동일한 확률로 값이 선택되도록 설정")
+        fun test_enableEqualDistribution() {
+            val passwordGenerator = PasswordGenerator(SecurityStrength.HIGH, SecureRandom())
+            passwordGenerator.enableEqualDistribution()
+            passwordGenerator.generate(8)
+            val generatedValue = passwordGenerator.getValue()
+
+            assertNotNull(generatedValue)
+            assertEquals(8, generatedValue.length)
+        }
+
+        @Test
+        @DisplayName("모든 CharGroup에서 동일하지 않은 확률로 값이 선택되지 않도록 설정")
+        fun test_disableEqualDistribution() {
+            val passwordGenerator = PasswordGenerator(SecurityStrength.HIGH, SecureRandom())
+            passwordGenerator.disableEqualDistribution()
+            passwordGenerator.generate(8)
+            val generatedValue = passwordGenerator.getValue()
+
+            assertNotNull(generatedValue)
+            assertEquals(8, generatedValue.length)
+        }
     }
 }
