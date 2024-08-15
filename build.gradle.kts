482f57d526bf782cc3ec9347415e2e3acf160132
yjj8353 <yjj8353@gmail.com>
2024-08-15 16:59:25 +0900

    sonar 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,20 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     java
+    jacoco
     kotlin("jvm") version "2.0.0"
     `maven-publish`
     id("org.jetbrains.dokka") version "1.9.20"
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
 group = "dev.retrotv"
-version = "0.10.0-alpha"
+version = "0.10.1-alpha"
 
 // Github Action 버전 출력용
 tasks.register("printVersionName") {
+    description = "이 프로젝트의 버전을 출력합니다."
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
     println(project.version)
 }
 
@@ -25,9 +29,9 @@ val junitVersion = "5.10.0"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.github.retrotv-maven-repo:data-utils:$dataUtilsVersion")
+    implementation("com.github.retrotv-maven-repo:data-utils:${dataUtilsVersion}")
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
 }
 
 tasks {
@@ -53,8 +57,29 @@ publishing {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
 }
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.jacocoTestReport {
+    reports {
+
+        // HTML 파일을 생성하도록 설정
+        html.required = true
+
+        // SonarQube에서 Jacoco XML 파일을 읽을 수 있도록 설정
+        xml.required = true
+        csv.required = false
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "retrotv-maven-repo_extended-file")
+        property("sonar.organization", "retrotv-maven-repo")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
